9ebab2f6dc18aed533c2df032a80431c083feda3
pierreclavier <pierre.clavier.c@gmail.com>
2023-10-10 10:27:59 +0200

    fr

--- run_logistic.py
+++ run_logistic.py
@@ -13,7 +13,7 @@ jax.config.update("jax_debug_nans", True)
 
 @hydra.main(config_path="parameters", config_name="logistic")
 def main(args: DictConfig):
-    wandb.init(config=dict(args), project=f"{args.env}_{args.agent_name}")
+    wandb.init(config=dict(args), project=f"{args.env}_{args.agent_name}_{args.context_noise}")
     game = Game(args)
     game.run_toy()
 
